9bc7609b49d6980c04ea97572349b0f21f3322a6
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2025-03-18 10:48:59 +1000

    adding a menu bar on the side

--- src/main.zig
+++ src/main.zig
@@ -14,13 +14,14 @@ pub fn main() !void {
     var server = try httpz.Server(*App).init(allocator, .{ .port = 8081 }, &app);
     var router = try server.router(.{});
     router.get("/", index, .{});
+    router.get("/bind", bind, .{});
     std.debug.print("Starting DataStar test app on http://localhost:8081\n", .{});
     try server.listen();
 }
 
 const App = struct {};
 
-pub fn fileServe(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+fn serveFile(filename: []const u8, arena: std.mem.Allocator, res: *httpz.Response) !void {
     errdefer {
         res.status = 404;
         res.body = "Not found";
@@ -28,16 +29,20 @@ pub fn fileServe(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
 
     // If the file exists in the public dir, then send it !
     const dir = try std.fs.cwd().openDir("www", .{});
-    const file = try dir.openFile(req.url.path[1..], .{});
+    const file = try dir.openFile(filename, .{});
     defer file.close();
 
-    res.body = try file.readToEndAlloc(req.arena, 1_000_000);
+    res.body = try file.readToEndAlloc(arena, 1_000_000);
 }
 
-fn index(app: *App, req: *httpz.Request, res: *httpz.Response) !void {
-    _ = app;
-    const dir = try std.fs.cwd().openDir("www", .{});
-    const file = try dir.openFile("index.html", .{});
-    defer file.close();
-    res.body = try file.readToEndAlloc(req.arena, 1_000_000);
+pub fn fileServe(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+    return serveFile(req.url.path[1..], req.arena, res);
+}
+
+fn index(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+    return serveFile("index.html", req.arena, res);
+}
+
+fn bind(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+    return serveFile("bind.html", req.arena, res);
 }
