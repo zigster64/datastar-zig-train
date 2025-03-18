10714df12808507f6e6d24bea1e82dc3f7241dc2
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2025-03-18 10:49:00 +1000

    having fun trying to get signals through from the req to the backend

--- build.zig
+++ build.zig
@@ -3,20 +3,25 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const dep_opts = .{ .target = target, .optimize = optimize };
 
     const exe = b.addExecutable(.{
         .name = "datastar_zig_train",
         .root_source_file = b.path("src/main.zig"),
-        .target = b.graph.host,
-    });
-
-    const httpz = b.dependency("httpz", .{
         .target = target,
         .optimize = optimize,
     });
-
-    exe.root_module.addImport("httpz", httpz.module("httpz"));
     b.installArtifact(exe);
+
+    const httpz = b.dependency("httpz", dep_opts).module("httpz");
+    exe.root_module.addImport("httpz", httpz);
+
+    // const datastar = b.dependency("datastar", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    //     // .framework = .httpz, // or .tokamak
+    // }).module("datastar");
+    // exe.root_module.addImport("datastar", datastar);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const httpz = @import("httpz");
+// const datastar = @import("datastar").httpz;
 
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{}).init;
@@ -14,7 +15,9 @@ pub fn main() !void {
     var server = try httpz.Server(*App).init(allocator, .{ .port = 8081 }, &app);
     var router = try server.router(.{});
     router.get("/", index, .{});
-    router.get("/bind", bind, .{});
+    router.get("/hello", hello, .{});
+    // router.get("/hello", helloWorld, .{});
+    // router.get("/bind", bind, .{});
     std.debug.print("Starting DataStar test app on http://localhost:8081\n", .{});
     try server.listen();
 }
@@ -43,6 +46,64 @@ fn index(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
     return serveFile("index.html", req.arena, res);
 }
 
-fn bind(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
-    return serveFile("bind.html", req.arena, res);
+pub fn readSignals(comptime T: type, req: *httpz.Request) !T {
+    switch (req.method) {
+        .GET => {
+            const query = try req.query();
+            const signals = query.get("datastar") orelse return error.MissingDatastarKey;
+            std.debug.print("got datastar param {s}\n", .{signals});
+
+            return std.json.parseFromSliceLeaky(T, req.arena, signals, .{});
+        },
+        else => {
+            const body = req.body() orelse return error.MissingBody;
+
+            return std.json.parseFromSliceLeaky(T, req.arena, body, .{});
+        },
+    }
+}
+
+// fn helloWorld(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+//     var sse = try datastar.ServerSentEventGenerator.init(res);
+//     const signals = try datastar.readSignals(
+//         Signals,
+//         req,
+//     );
+
+//     inline for (message, 0..) |_, i| {
+//         const fragment = std.fmt.comptimePrint(
+//             "<div id='message'>{s}</div>",
+//             .{message[0 .. i + 1]},
+//         );
+//         try sse.mergeFragments(fragment, .{});
+
+//         std.Thread.sleep(std.time.ns_per_ms * signals.delay);
+//     }
+// }
+
+const message = "Hello, world!";
+const HelloContext = struct {
+    delay: u64 = 1000,
+
+    fn handle(self: HelloContext, stream: std.net.Stream) void {
+        std.debug.print("in the handler with delay {}\n", .{self.delay});
+        const w = stream.writer();
+        w.print("event: datastar-merge-fragments\n", .{}) catch return;
+        inline for (message, 0..) |_, i| {
+            const fragment = std.fmt.comptimePrint(
+                "<div id='message'>{s}</div>",
+                .{message[0 .. i + 1]},
+            );
+            w.print("data: {s}\n", .{fragment}) catch return;
+
+            std.Thread.sleep(std.time.ns_per_ms * self.delay);
+        }
+        w.writeAll("\n\n") catch return;
+    }
+};
+
+fn hello(_: *App, req: *httpz.Request, res: *httpz.Response) !void {
+    const ctx = try readSignals(HelloContext, req);
+    std.debug.print("parsed a hello context with delay {}\n", .{ctx.delay});
+    try res.startEventStream(ctx, HelloContext.handle);
 }
